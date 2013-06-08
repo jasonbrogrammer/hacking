4e623da154fafa2ff02b964b1d7efd6fd2b93b08
jasonbrogrammer <jason.n.chang@gmail.com>
2013-06-08 15:42:03 -0700

    first commit

---
+++ helloWorld.m
@@ -0,0 +1,16 @@
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+
+
+int main(int argc, const char * argv[] ) {
+
+//NSLog(@"Hello world!");
+
+//NSString* myString = [NSString string];
+
+//NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+        NSLog (@"Hello, World!");
+//        [pool drain];
+        return 0;
+
+}
